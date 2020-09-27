0a2c7e47e98163865d7ad0f1c422619e480fb7a7
NuBiFoRM <nubiform@NuBiFoRMs-iMac.local>
2020-09-27 22:51:51 +0900

    20200927 todo-app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import TodoInsert from './components/TodoInsert';
+import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
   return (
     <div>
-      
+      <TodoTemplate>
+        <TodoInsert></TodoInsert>
+      </TodoTemplate>
     </div>
   );
 };
