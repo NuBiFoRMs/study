cad418fcb3072b741670bc3ad043ac3bfdac30a8
NuBiFoRM <nubiform@me.com>
2021-03-26 23:58:13 +0900

    테스트 케이스 작성

--- baseball/src/test/java/com/nubiform/baseball/BaseballGameTest.java
+++ baseball/src/test/java/com/nubiform/baseball/BaseballGameTest.java
@@ -33,4 +33,44 @@ public class BaseballGameTest {
 
         assertEquals("invalid argument", exception.getMessage());
     }
+
+    @Test
+    public void 입력_값의_자리_수가_세_자리가_아닐_경우() {
+
+    }
+
+    @Test
+    public void 입력_값에_숫자_외의_문자가_입력될_경우() {
+
+    }
+
+    @Test
+    public void 입력_값에_중복된_숫자가_입력될_경우() {
+
+    }
+
+    @Test
+    public void 숫자_세_개가_전부_일치하는_경우() {
+
+    }
+
+    @Test
+    public void 숫자_세_개가_전부_일치하지_않을_경우() {
+
+    }
+
+    @Test
+    public void 스트라이크만_있는_경우() {
+
+    }
+
+    @Test
+    public void 볼만_있는_경우() {
+
+    }
+
+    @Test
+    public void 볼과_스트라이크가_함께_있는_경우() {
+
+    }
 }
