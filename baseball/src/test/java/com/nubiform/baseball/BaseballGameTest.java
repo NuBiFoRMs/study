1d4d30c115b605e63758097bbf0a1a98106bab73
NuBiFoRM <nubiform@me.com>
2021-03-26 21:46:32 +0900

    init project junit jupiter

---
+++ baseball/src/test/java/com/nubiform/baseball/BaseballGameTest.java
@@ -0,0 +1,13 @@
+package com.nubiform.baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class BaseballGameTest {
+
+    @Test
+    public void test() {
+        fail();
+    }
+}
