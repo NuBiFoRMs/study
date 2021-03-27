64042d16f2fc98b605cc8f49082d9c32f7ffc465
NuBiFoRM <nubiform@me.com>
2021-03-27 12:33:50 +0900

    BaseballGame 구현

--- baseball/src/main/java/com/nubiform/baseball/BaseballGame.java
+++ baseball/src/main/java/com/nubiform/baseball/BaseballGame.java
@@ -1,5 +1,10 @@
 package com.nubiform.baseball;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class BaseballGame {
     public BaseballGame(String answer) {
     }
@@ -10,6 +15,15 @@ public class BaseballGame {
 
         if (!question.matches("^[0-9]{3}$]"))
             throw new IllegalArgumentException("invalid argument");
+
+        Map<String, Integer> duplicateCheck = new HashMap<>();
+        for (int i = 0; i < question.length(); i++) {
+            String charString = String.valueOf(question.charAt(i));
+            if (duplicateCheck.containsKey(charString))
+                throw new IllegalArgumentException("invalid argument");
+            else
+                duplicateCheck.put(charString, 0);
+        }
 
         return null;
     }
