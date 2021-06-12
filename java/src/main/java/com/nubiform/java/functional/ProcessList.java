dcb1402ed374b90a272a6f8485c7d14184095d87
NuBiFoRM <nubiform@me.com>
2021-06-12 19:35:48 +0900

    functional

--- java/src/main/java/com/nubiform/java/functional/ProcessList.java
+++ java/src/main/java/com/nubiform/java/functional/ProcessList.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class ProcessList {
 
