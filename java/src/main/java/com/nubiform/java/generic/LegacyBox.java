335e2ece8180d7bed5c39aa9f21c438224c6bf3d
NuBiFoRM <nubiform@me.com>
2021-06-29 16:35:18 +0900

    java generic

---
+++ java/src/main/java/com/nubiform/java/generic/LegacyBox.java
@@ -0,0 +1,14 @@
+package com.nubiform.java.generic;
+
+public class LegacyBox {
+
+    private Object box;
+
+    public Object get() {
+        return box;
+    }
+
+    public void set(Object box) {
+        this.box = box;
+    }
+}
---
+++ java/src/main/java/com/nubiform/java/generic/Orange.java
@@ -0,0 +1,9 @@
+package com.nubiform.java.generic;
+
+public class Orange {
+
+    @Override
+    public String toString() {
+        return "I am an orange.";
+    }
+}
